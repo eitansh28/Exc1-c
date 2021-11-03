f32cc726f2edb08209c317e38d90d17a2bf9546e
eitansh28 <eitan76543@gmail.com>
2021-11-03 07:45:50 +0200

    bokertov

--- main.c
+++ main.c
@@ -5,25 +5,25 @@
 int main() {
 	int x,y;
 	scanf("%d %d",&x,&y);
-	printf("The Armstrong numbers are: ");
+	printf("The Armstrong numbers are:");
 	for(int i=x;i<=y;i++){
 		if(isArmstrong(i))
-			printf("%d ",i);
+			printf(" %d",i);
 	}
-	printf("\nThe Palindromes numbers are: ");
+	printf("\nThe Palindromes numbers are:");
 	for(int i=x;i<=y;i++){
 		if(isPalindrome(i))
-			printf("%d ",i);
+			printf(" %d",i);
 	}
-	printf("\nThe Prime numbers are: ");
+	printf("\nThe Prime numbers are:");
 	for(int i=x;i<=y;i++){
 		if(isPrime(i))
-			printf("%d ",i);
+			printf(" %d",i);
 	}
-	printf("\nThe Strong numbers are: ");
+	printf("\nThe Strong numbers are:");
 	for(int i=x;i<=y;i++){
 		if(isStrong(i))
-			printf("%d ",i);
+			printf(" %d",i);
 	}
 	printf("\n");
 	return 0;
